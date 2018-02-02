3c801c48740e49e8e2038c5d446cadba400f19ba
Jérôme Martin <euhmeuh@users.noreply.github.com>
2018-02-02 14:54:28 +0100

    add missing build deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "command-tree")
 (define deps '("base"))
-(define build-deps '())
+(define build-deps '("rackunit-lib"))
 (define version "0.1")
 (define pkg-authors '(euhmeuh))
